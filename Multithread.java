50aa5a91165a44366fbbd332b562af2217d37378
Shreyank isiri <76459138+Shreyankthehacker@users.noreply.github.com>
2024-08-29 18:36:04 +0530

    Anither way for m threading

--- Multithread.java
+++ Multithread.java
@@ -2,21 +2,19 @@ public class Multithread {
   public static void main(String[] args) {
      Process1 p1 = new Process1();
      Process2 p2 = new Process2();
-     Thread t1 = new Thread(p1);
-     Thread t2 = new Thread(p2);
-     t1.start();
-     t2.start();
+     p1.start();
+     p2.start();
   }
 }
 
-class Process1 implements Runnable {
+class Process1 extends Thread {
   @Override
   public void run() {
      for (int i = 0; i < 10; i++) System.out.println(i);
   }
 }
 
-class Process2 implements Runnable {
+class Process2 extends Thread {
   @Override
   public void run() {
      for (int i = 0; i < 10; i++) System.out.println(i);
